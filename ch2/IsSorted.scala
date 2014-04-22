f30d475c3e4054de2ec1f8a71fd7b0594d2fbd9c
Cevaris <cevaris@gmail.com>
2014-04-22 16:30:12 -0600

    created sample sort check for generic objects

--- ch2/IsSorted.scala
+++ ch2/IsSorted.scala
@@ -4,7 +4,7 @@ def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
   def go(curr: Int, next: Int, flag: Boolean): Boolean = {
     if(as.length == next) flag
     else {
-      println(s"Curr:${as(curr)} <= Next:${as(next)} = ${gt(as(curr), as(next))}")
+      // println(s"Curr:${as(curr)} <= Next:${as(next)} = ${gt(as(curr), as(next))}")
       if(!gt(as(curr), as(next))) false
       else go(next, next+1, flag)
     }
@@ -15,12 +15,49 @@ def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
 }
 
 
-val empty = Array[Int]()
-println(isSorted(empty, (x:Int,y:Int) => x <= y))
+// val empty = Array[Int]()
+// println(isSorted(empty, (x:Int,y:Int) => x <= y))
 
-val notSorted = Array[Int](1,2,5,8,3,4,5,1)
-println(isSorted(notSorted, (x:Int,y:Int) => x <= y))
+// val single = Array[Int](1)
+// println(isSorted(single, (x:Int,y:Int) => x <= y))
 
-val sorted = notSorted.sortWith(_ < _)
-println(isSorted(sorted, (x:Int,y:Int) => x <= y))
+
+// val notSorted = Array[Int](1,2,5,8,3,4,5,1)
+// println(isSorted(notSorted, (x:Int,y:Int) => x <= y))
+
+// val sorted = notSorted.sortWith(_ < _)
+// println(isSorted(sorted, (x:Int,y:Int) => x <= y))
+
+
+// val empty = Array[Double]()
+// println(isSorted(empty, (x:Double,y:Double) => x <= y))
+
+// val single = Array[Double](1.3)
+// println(isSorted(single, (x:Double,y:Double) => x <= y))
+
+
+// val notSorted = Array[Double](1.0,2.0,5.0,8.0,3.9,4.9,5,1.3)
+// println(isSorted(notSorted, (x:Double,y:Double) => x <= y))
+
+// val sorted = notSorted.sortWith(_ < _)
+// println(isSorted(sorted, (x:Double,y:Double) => x <= y))
+
+
+
+case class Player(name: String, score: Double) {
+  override def toString(): String = name
+}
+
+val empty = Array[Player]()
+println(isSorted(empty, (x:Player,y:Player) => x.score <= y.score))
+
+val single = Array[Player](Player("Tom", 7.9))
+println(isSorted(single, (x:Player,y:Player) => x.score <= y.score))
+
+
+val notSorted = Array[Player](Player("Sue", 7.1),Player("Charles", 3.3),Player("Will", 3.4))
+println(isSorted(notSorted, (x:Player,y:Player) => x.score <= y.score))
+
+val sorted = notSorted.sortWith(_.score < _.score)
+println(isSorted(sorted, (x:Player,y:Player) => x.score <= y.score))
 
